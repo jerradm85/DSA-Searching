4014830645bbfff7a600f1541b9936cd667c20bc
Jerrad Moon <jerradm85@gmail.com>
2020-11-28 13:35:38 -0800

    first commit

---
+++ binary-search.js
@@ -0,0 +1,34 @@
+function binarySearch(array, value, start=0, end=array.length-1) {
+
+  if (start > end) {
+      return -1;
+  }
+
+  const index = Math.floor((start + end) / 2);
+  const item = array[index];
+
+  console.log(start, end);
+  if (item == value) {
+      return index;
+  }
+  else if (item < value) {
+      return binarySearch(array, value, index + 1, end);
+  }
+  else if (item > value) {
+      return binarySearch(array, value, start, index - 1);
+  }
+}
+
+const arrBin = [89, 30, 25, 32, 72, 70, 51, 42, 25, 24, 53, 55, 78, 50, 13, 40, 48, 32, 26, 2, 14, 33, 45, 72, 56, 44, 21, 88, 27, 68, 15, 62, 93, 98, 73, 28, 16, 46, 87, 28, 65, 38, 67, 16, 85, 63, 23, 69, 64, 91, 9, 70, 81, 27, 97, 82, 6, 88, 3, 7, 46, 13, 11, 64, 76, 31, 26, 38, 28, 13, 17, 69, 90, 1, 6, 7, 64, 43, 9, 73, 80, 98, 46, 27, 22, 87, 49, 83, 6, 39, 42, 51, 54, 84, 34, 53, 78, 40, 14, 5];
+
+const sort = arrBin.sort(function(a, b) { return a - b; });
+let uniqueNums = [...new Set(sort)];
+console.log(uniqueNums);
+
+const binaryElement = document.querySelector('.binary');
+binaryElement.addEventListener('change', (event) => {
+  const display = document.querySelector('#binary-display');
+  const input = binarySearch(uniqueNums, event.target.value)
+  
+  display.textContent = input;
+});
